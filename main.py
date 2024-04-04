fc32ecf649180ae63901cf85d76e00f67b534e2d
Chenrui Fan <chenrui_fan@outlook.com>
2024-04-04 18:09:05 +0200

    update

--- main.py
+++ main.py
@@ -5,22 +5,60 @@ import queue
 
 
 def test_1(memo1, memo2):
-
     memo1.merge(memo2)
-
-    # show memos
     cv2.imshow("memo1", memo1.get_pic())
     cv2.imshow("memo2", memo2.get_pic())
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-def test_2(memo1, memo2, detector, audio_pipe):
+def merge(memo1, memo2, memo_1_detect, memo_2_detect, audio_done_event, last_audio_trigger_time, audio_trigger_interval, result_queue, audio_pipe):
+    current_time = time.time()
+    if memo_1_detect and memo_2_detect and not audio_done_event.is_set() and (current_time - last_audio_trigger_time > audio_trigger_interval):
+        print('Now start audio recognition:')
+        last_audio_trigger_time = current_time
+        audio_done_event.clear()
+        audio_thread = threading.Thread(target=audio_trigger_merge, args=(audio_pipe, result_queue, audio_done_event))
+        audio_thread.start()
+    if audio_done_event.is_set():
+        recognition_result = result_queue.get()
+        if recognition_result:
+            print('Now using audio to merge memo:')
+            memo1.merge(memo2)
+            print('Merge is done!')
+            print('----------------------------------')
+        audio_done_event.clear()
+    return audio_done_event, last_audio_trigger_time, result_queue
+
+
+def create(audio_done_event, last_audio_trigger_time, audio_trigger_interval, result_queue, audio_pipe):
+    current_time = time.time()
+    memo_new = False
+    if not audio_done_event.is_set() and (current_time - last_audio_trigger_time > audio_trigger_interval):
+        print('Now start audio recognition:')
+        last_audio_trigger_time = current_time
+        audio_done_event.clear()
+        audio_thread = threading.Thread(target=audio_trigger_create, args=(audio_pipe, result_queue, audio_done_event))
+        audio_thread.start()
+    if audio_done_event.is_set():
+        recognition_result = result_queue.get()
+        if recognition_result:
+            print('Now using audio to create memo:')
+            memo_new = Memo([200, 200, 200], "NEW", side="right")
+            print('Create is done!')
+            print('----------------------------------')
+        audio_done_event.clear()
+    return memo_new, audio_done_event, last_audio_trigger_time, result_queue
+
+
+def start(memo1, memo2, detector, audio_pipe):
     cap = cv2.VideoCapture(0)
-    audio_done_event = threading.Event()
-    result_queue = queue.Queue()
-    last_audio_trigger_time = 0  # 上一次触发录音的时间戳
-    audio_trigger_interval = 3  # 允许触发录音的最小时间间隔（秒）
+    audio_done_event_merge = threading.Event()
+    audio_done_event_create = threading.Event()
+    result_queue_merge = queue.Queue()
+    result_queue_create = queue.Queue()
+    last_audio_trigger_time = 0
+    audio_trigger_interval = 3
     while True:
         ret, frame = cap.read()
         if ret:
@@ -36,23 +74,13 @@ def test_2(memo1, memo2, detector, audio_pipe):
                 if memo2.is_triggered(detection_result, frame):
                     highlight_memo(frame, memo2)
                     memo_2_detect = True
-                    
-                current_time = time.time()
-                if memo_1_detect and memo_2_detect and not audio_done_event.is_set() and (current_time - last_audio_trigger_time > audio_trigger_interval):
-                    print('Now start audio recognition:')
-                    last_audio_trigger_time = current_time  # 更新上一次触发录音的时间
-                    audio_done_event.clear()
-                    audio_thread = threading.Thread(target=audio_trigger, args=(audio_pipe, result_queue, audio_done_event))
-                    audio_thread.start()
-                    
-                if audio_done_event.is_set():
-                    recognition_result = result_queue.get()  # 安全地获取结果
-                    if recognition_result:
-                        print('Now using audio to merge:')
-                        memo1.merge(memo2)
-                    audio_done_event.clear()
-
+                # Merge memo function
+                audio_done_event_merge, last_audio_trigger_time, result_queue_merge = merge(memo1, memo2, memo_1_detect, memo_2_detect, audio_done_event_merge, last_audio_trigger_time, audio_trigger_interval, result_queue_merge, audio_pipe)
+                # Create memo function
+                memo_new, audio_done_event_create, last_audio_trigger_time, result_queue_create = create(audio_done_event_create, last_audio_trigger_time, audio_trigger_interval, result_queue_create, audio_pipe)
             frame = add_memo(frame, memo1)
+            if memo_new:
+                memo2 = memo_new
             frame = add_memo(frame, memo2)
             cv2.imshow("camera", frame)
         # press 'q' to exit
@@ -61,12 +89,14 @@ def test_2(memo1, memo2, detector, audio_pipe):
     cap.release()
     cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
 
-    # initialize audio_model
+    # initialize
     model_id = ["openai/whisper-base", "openai/whisper-large-v3"]
     audio_pipe = model_initialize(model_id[0])
     memo1 = Memo([200, 30, 30], "A", side="left")
     memo2 = Memo([30, 30, 200], "B", side="right")
     detector = detector_init()
-    test_2(memo1, memo2, detector, audio_pipe)
+
+    start(memo1, memo2, detector, audio_pipe)
--- speech2txt/main.py
+++ speech2txt/main.py
@@ -3,7 +3,7 @@ from .totxt import *
 import time
 from datetime import datetime
 
-def audio_trigger(pipe, result_queue, done_event):
+def audio_trigger_merge(pipe, result_queue, done_event):
     start_time = datetime.now()
     byte_io = record(duration = 1)
     print(f"record time: {(datetime.now() - start_time).total_seconds()} seconds")
@@ -11,6 +11,19 @@ def audio_trigger(pipe, result_queue, done_event):
     print(f"to text time: {(datetime.now() - start_time).total_seconds()} seconds")
     print('text: ', text)
     if "m" in text.lower() or "g" in text.lower():
+        result_queue.put(True)
+        print(f"total time: {(datetime.now() - start_time).total_seconds()} seconds")
+    result_queue.put(False)
+    done_event.set()
+
+def audio_trigger_create(pipe, result_queue, done_event):
+    start_time = datetime.now()
+    byte_io = record(duration = 1)
+    print(f"record time: {(datetime.now() - start_time).total_seconds()} seconds")
+    text = speech2txt(pipe, sample=byte_io.read())
+    print(f"to text time: {(datetime.now() - start_time).total_seconds()} seconds")
+    print('text: ', text)
+    if "c" in text.lower():
         result_queue.put(True)
         print(f"total time: {(datetime.now() - start_time).total_seconds()} seconds")
     result_queue.put(False)
