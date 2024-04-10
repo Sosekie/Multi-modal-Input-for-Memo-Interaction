e8b34ca65137481e671479ea9d27e1a6ff00fe20
FuQianshi <72088061+Sosekie@users.noreply.github.com>
2024-04-10 17:05:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def merge(memo1, memo2, memo_1_detect, memo_2_detect, audio_done_event, last_aud
     return audio_done_event, last_audio_trigger_time, result_queue
 
 
-def create(audio_done_event, last_audio_trigger_time, audio_trigger_interval, result_queue, audio_pipe):
+def create(position, audio_done_event, last_audio_trigger_time, audio_trigger_interval, result_queue, audio_pipe):
     current_time = time.time()
     memo_new = False
     if not audio_done_event.is_set() and (current_time - last_audio_trigger_time > audio_trigger_interval):
@@ -53,6 +53,7 @@ def create(audio_done_event, last_audio_trigger_time, audio_trigger_interval, re
 
 def start(memo1, memo2, detector, audio_pipe):
     cap = cv2.VideoCapture(0)
+    # threading event here
     audio_done_event_merge = threading.Event()
     audio_done_event_create = threading.Event()
     result_queue_merge = queue.Queue()
@@ -67,30 +68,33 @@ def start(memo1, memo2, detector, audio_pipe):
             detection_result = detector.detect(mp_image)
             if detection_result:
                 frame = draw_landmarks_on_image(mp_image.numpy_view(), detection_result)
+                
+                # Merge memo function
                 memo_1_detect, memo_2_detect = False, False
                 if memo1.is_triggered(detection_result, frame):
                     highlight_memo(frame, memo1)
                     memo_1_detect = True
-                
                 if memo2.is_triggered(detection_result, frame):
                     highlight_memo(frame, memo2)
                     memo_2_detect = True
-                # Merge memo function
                 audio_done_event_merge, last_audio_trigger_time, result_queue_merge = merge(memo1, memo2, memo_1_detect, memo_2_detect, audio_done_event_merge, last_audio_trigger_time, audio_trigger_interval, result_queue_merge, audio_pipe)
+                
                 # Create memo function
-                memo_new, audio_done_event_create, last_audio_trigger_time, result_queue_create = create(audio_done_event_create, last_audio_trigger_time, audio_trigger_interval, result_queue_create, audio_pipe)
+                memo_new = None
+                if create_flag:
+                    # position = # get position here,
+                    memo_new, audio_done_event_create, last_audio_trigger_time, result_queue_create = create(position, audio_done_event_create, last_audio_trigger_time, audio_trigger_interval, result_queue_create, audio_pipe)
 
-                if memo3.is_pinched(detection_result, frame):
-                    memo1.handle_pinch(detection_result, frame)
-                    highlight_memo(frame, memo1)
-                    memo_1_detect = True
-                    memo1.position_x = fingerp
+                # if memo3.is_pinched(detection_result, frame):
+                #     memo1.handle_pinch(detection_result, frame)
+                #     highlight_memo(frame, memo1)
+                #     memo_1_detect = True
+                #     memo1.position_x = fingerp
 
             frame = add_memo(frame, memo1)
-            if memo_new:
-                memo2 = memo_new
             frame = add_memo(frame, memo2)
-            frame = add_memo(frame, memo3)
+            if memo_new:
+                frame = add_memo(frame, memo_new)
             cv2.imshow("camera", frame)
         # press 'q' to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
